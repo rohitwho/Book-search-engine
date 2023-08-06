bc03c0d6cd538cf9f0cf6335a37466510c70503a
rohit nayyar <rohit@rohits-MacBook-Air.local>
2023-08-05 20:16:27 -0400

    connected with mongo atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect("mongodb+srv://rohitnayyar54:160298rn!@book-search-engine.povauw9.mongodb.net/");
 
 module.exports = mongoose.connection;
+
+
+
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
